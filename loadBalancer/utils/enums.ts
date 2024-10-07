bbb490e7b3f22cce4af34bc4f73b5108b423c3ec
ravikishan.69 <ravikishan63392@gmail.com>
2024-10-07 22:49:30 +0530

    Initial Commit

---
+++ loadBalancer/utils/enums.ts
@@ -0,0 +1,11 @@
+// Enumerations for Load Balancer Algorithm
+export enum LbAlgorithm {
+    RANDOM = 'rand',
+    ROUND_ROBIN = 'rr',
+    WEIGHTED_ROUND_ROBIN = 'wrr',
+}
+
+export enum BEServerHealth {
+    UNHEALTHY = 'un_healthy',
+    HEALTHY = 'healthy'
+}
